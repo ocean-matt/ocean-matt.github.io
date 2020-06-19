f3a69e0d8ee270e2c9fe5e57945fac82f91bd9b1
ocean-matt <43844916+ocean-matt@users.noreply.github.com>
2020-06-19 15:46:52 +0100

    Added HTML content and some php pages

---
+++ navbar.php
@@ -0,0 +1,42 @@
+
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-md navbar-custom"> <!--bg-dark navbar-dark-->
+
+  <!-- Toggler/collapsibe Button -->
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+<!-- Navigation bar at top -->
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="research.php">Research</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="personal.php">Personal</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php">CV & Contact</a>
+      </li>
+    </ul>
+   </div>
+   <div class="navbar-collapse collapse">
+      <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.twitter.com/Ocean_MattC/">
+		<i class="fab fa-twitter" style="font-size:30px"></i>
+		</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/ocean-matt">
+		<i class="fab fa-github" style="font-size:30px"></i>
+		</a>
+            </li>
+        </ul>
+    </div>    
+  </div>
+</nav>
